e6547de7bc13d0f0c1756a726f51f4709ef08766
dy <dy472654243@qq.com>
2019-08-22 16:03:21 +0800

    模特详情

---
+++ lib/pages/model/model_detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ModelDetail extends StatefulWidget {
+  @override
+  _ModelDetailState createState() => _ModelDetailState();
+}
+
+class _ModelDetailState extends State<ModelDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
