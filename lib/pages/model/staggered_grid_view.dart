beafec4ffc58d823a37b53398c884f8e30bc9fc6
dy <dy472654243@qq.com>
2019-08-22 09:09:14 +0800

    模特列表

--- lib/pages/model/staggered_grid_view.dart
+++ lib/pages/model/staggered_grid_view.dart
@@ -3,11 +3,9 @@ import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:mote/model_data/model/modeListModel.dart';
 import 'package:provide/provide.dart';
 import '../../provide/model.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class StaggeredGrid extends StatefulWidget {
-  final List modelList;
-  const StaggeredGrid({Key key, this.modelList}) : super(key: key);
-
   @override
   _StaggeredGridState createState() => _StaggeredGridState();
 }
@@ -21,50 +19,44 @@ class _StaggeredGridState extends State<StaggeredGrid>
 
   var posts = [];
 
-  // @override
-  // void initState() {
-  //   _loadData();
-  //   _scrollController.addListener(() {
-  //     if (_scrollController.position.pixels ==
-  //         _scrollController.position.maxScrollExtent) {
-  //       _loadData(loadMore: true);
-  //     }
-  //   });
-  //   super.initState();
-  // }
-
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
+    super.build(context);
     return Scaffold(
-        body: RefreshIndicator(
-      onRefresh: _handleRefresh,
-      child: MediaQuery.removePadding(
-        removeTop: true,
-        context: context,
-        child: new StaggeredGridView.countBuilder(
-          controller: _scrollController,
-          crossAxisCount: 4,
-          itemCount: 19,
-          primary: false,
-          itemBuilder: (BuildContext context, int index) => new Container(
-              color: Colors.green,
-              child: new Center(
-                child: new CircleAvatar(
-                  backgroundColor: Colors.white,
-                  child: new Text('$index'),
-                ),
-              )),
-          staggeredTileBuilder: (int index) => new StaggeredTile.count(2, index.isEven ? 2 : 1),
-          mainAxisSpacing: 4.0,
-          crossAxisSpacing: 4.0,
-        ),
-      ),
+        body: Container(
+      child: FutureBuilder(
+          future: _getModelInfo(context),
+          builder: (context, snapsshot) {
+            if (snapsshot.hasData) {
+
+              return RefreshIndicator(
+                onRefresh: _handleRefresh,
+                child: MediaQuery.removePadding(
+                    removeTop: true,
+                    context: context,
+                    child: Provide<ModelProvide>(
+                      builder: (context, child, val) {
+                         var cardItem = Provide.value<ModelProvide>(context).list;
+                        return StaggeredGridView.countBuilder(
+                          controller: _scrollController,
+                          crossAxisCount: 4,
+                          itemCount: cardItem?.length ?? 0,
+                          itemBuilder: (BuildContext context, int index) =>
+                              TileCard(
+                                  headUrl: '${cardItem[index].headUrl}',
+                                  name: '${cardItem[index].name}'),
+                          staggeredTileBuilder: (int index) =>
+                              new StaggeredTile.fit(2),
+                          mainAxisSpacing: 4.0,
+                          crossAxisSpacing: 4.0,
+                        );
+                      },
+                    )),
+              );
+            } else {
+              return Container();
+            }
+          }),
     ));
   }
 
@@ -77,9 +69,30 @@ class _StaggeredGridState extends State<StaggeredGrid>
     } else {
       pageIndex = 1;
     }
+    _getModelInfo(context);
+  }
 
-    Provide.value<ModelProvide>(context)
-        .getmodelList({'pageNum': pageIndex, 'roleName': 2});
+  Future _getModelInfo(BuildContext context) async {
+    Provide.value<ModelProvide>(context).getmodelList(pageIndex);
+    return '完成加载';
+  }
+
+  @override
+  void initState() {
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _loadData(loadMore: true);
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+
+    super.dispose();
   }
 }
 
@@ -87,33 +100,55 @@ Future<Null> _handleRefresh() async {
   return null;
 }
 
-class _ModelItem extends StatelessWidget {
-  final ModelList item;
-  final int index;
+class TileCard extends StatelessWidget {
+  final String headUrl;
+  final String name;
 
-  const _ModelItem({Key key, this.item, this.index}) : super(key: key);
+  TileCard({this.headUrl, this.name});
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {},
-      child: Card(
-        child: PhysicalModel(
-          color: Colors.transparent,
-          clipBehavior: Clip.antiAlias,
-          borderRadius: BorderRadius.circular(5),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[_itemImage()],
+    return Card(
+      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8.0))),
+      child: Column(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius:BorderRadius.only(topLeft: Radius.circular(8.0),topRight: Radius.circular(8.0)),
+            child: Image(
+              image:NetworkImage('$headUrl'),
+              fit: BoxFit.fitWidth,
+              width: 200,
+            )
           ),
-        ),
+          Container(
+            padding: EdgeInsets.only(
+                left: ScreenUtil().setWidth(20),
+                bottom: ScreenUtil().setWidth(20),
+                top: ScreenUtil().setWidth(20)
+              ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: NetworkImage('$headUrl'),
+                  radius: ScreenUtil().setWidth(30),
+                  // maxRadius: 40.0,
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: ScreenUtil().setWidth(20)),
+                  width: ScreenUtil().setWidth(250),
+                  child: Text(
+                    '$name',
+                    style: TextStyle(
+                      fontSize: ScreenUtil().setSp(25),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
-    );
-  }
-
-  _itemImage() {
-    return Image(
-      image: NetworkImage(item.headUrl),
     );
   }
 }
--- lib/provide/model.dart
+++ lib/provide/model.dart
@@ -6,13 +6,25 @@ import 'dart:convert';
 
 class ModelProvide with ChangeNotifier {
   ModelListModel modeListData;
+  List<ModelList> list;
+  bool isEnd = false;
 
   //获取模特列表
-  getmodelList(params) async {
-    FormData formData = FormData.from(params);
+  getmodelList(int pageIndex) async {
+    if (isEnd) {
+      return;
+    }
+    var formData = {'pageNum': pageIndex, 'roleName': 2};
     await requestGet('getmodelList', formData: formData).then((val) {
       modeListData = ModelListModel.fromJson(val);
-      print(val);
+
+      if (list != null) {
+        list.addAll(modeListData.data.pageInfo.list);
+      } else {
+        print(1);
+        list = modeListData.data.pageInfo.list;
+      }
+      isEnd = modeListData.data.pageInfo.isLastPage;
       notifyListeners();
     });
   }
