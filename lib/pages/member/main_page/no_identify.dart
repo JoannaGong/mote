5d8a49b4146bcdb1d3270892cb55599722b3b59f
Joanna <gkj159@163.com>
2019-08-27 21:15:35 +0800

    no identify

--- lib/pages/member/main_page/no_identify.dart
+++ lib/pages/member/main_page/no_identify.dart
@@ -5,22 +5,82 @@ class NoIdentify extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Color(0xFFF5F5F5),
-      padding: EdgeInsets.symmetric(horizontal: ScreenUtil().setWidth(28), vertical: ScreenUtil().setHeight(40)),
-      alignment: Alignment.centerLeft,
+      margin: EdgeInsets.symmetric(horizontal: ScreenUtil().setWidth(28), vertical: ScreenUtil().setHeight(40)),
       child: Column(children: <Widget>[
-        Text('认证之后才可开通附加功能', style: TextStyle(fontSize: ScreenUtil().setSp(24), color: Color(0xFF333333)), textAlign: TextAlign.left,),
+        Container(alignment: Alignment.centerLeft, child: Text('认证之后才可开通附加功能', style: TextStyle(fontSize: ScreenUtil().setSp(24), color: Color(0xFF333333)))),
         Container(
           height: ScreenUtil().setHeight(162),
-          decoration: BoxDecoration(gradient: LinearGradient(colors: [Color(0xFF9FFFDA), Color(0xFF00AEEF)], begin: FractionalOffset(0, 0), end: FractionalOffset(1, 1))),
-          padding: EdgeInsets.symmetric(horizontal: ScreenUtil().setWidth(32), vertical: ScreenUtil().setHeight(20)),
+          width: ScreenUtil().setWidth(694),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(ScreenUtil().setWidth(12))),
+            gradient: LinearGradient(colors: [Color(0xFF00AEEF), Color(0xFF9FFFDA)], begin: FractionalOffset(0, 0), end: FractionalOffset(1, 1))
+          ),
+          padding: EdgeInsets.symmetric(horizontal: ScreenUtil().setWidth(32), vertical: ScreenUtil().setHeight(15)),
           margin: EdgeInsets.symmetric(vertical: ScreenUtil().setHeight(20)),
           child: Column(children: <Widget>[
-            Expanded(child: Text('认证商家', style: TextStyle(fontSize: ScreenUtil().setSp(32), color: Colors.white))),
-            Expanded(child: Text('附加功能：发布通告、定向预约', style: TextStyle(fontSize: ScreenUtil().setSp(20), color: Colors.white), textAlign: TextAlign.left,),)
-          ],),
+            Expanded(child: Container(alignment: Alignment.centerLeft, child: Text('认证商家', style: TextStyle(fontSize: ScreenUtil().setSp(34), color: Colors.white)))),
+            Expanded(child: Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.only(top: ScreenUtil().setHeight(10)),
+              child: Text('附加功能：发布通告、定向预约', style: TextStyle(fontSize: ScreenUtil().setSp(24), color: Colors.white)))
+            )
+          ])
+        ),
+        Container(
+          height: ScreenUtil().setHeight(162),
+          width: ScreenUtil().setWidth(694),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(ScreenUtil().setWidth(12))),
+            gradient: LinearGradient(colors: [Color(0xFFFF3D3D), Color(0xFFFFC28C)], begin: FractionalOffset(0, 0), end: FractionalOffset(1, 1))
+          ),
+          padding: EdgeInsets.symmetric(horizontal: ScreenUtil().setWidth(32), vertical: ScreenUtil().setHeight(15)),
+          margin: EdgeInsets.symmetric(vertical: ScreenUtil().setHeight(20)),
+          child: Column(children: <Widget>[
+            Expanded(child: Container(alignment: Alignment.centerLeft, child: Text('认证艺人', style: TextStyle(fontSize: ScreenUtil().setSp(34), color: Colors.white)))),
+            Expanded(child: Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.only(top: ScreenUtil().setHeight(10)),
+              child: Text('附加功能：申请通告、定向预约', style: TextStyle(fontSize: ScreenUtil().setSp(24), color: Colors.white)))
+            )
+          ])
+        ),
+        Container(
+          height: ScreenUtil().setHeight(162),
+          width: ScreenUtil().setWidth(694),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(ScreenUtil().setWidth(12))),
+            gradient: LinearGradient(colors: [Color(0xFFDA83E9), Color(0xFF6B5BFE)], begin: FractionalOffset(0, 0), end: FractionalOffset(1, 1))
+          ),
+          padding: EdgeInsets.symmetric(horizontal: ScreenUtil().setWidth(32), vertical: ScreenUtil().setHeight(15)),
+          margin: EdgeInsets.symmetric(vertical: ScreenUtil().setHeight(20)),
+          child: Column(children: <Widget>[
+            Expanded(child: Container(alignment: Alignment.centerLeft, child: Text('认证经纪', style: TextStyle(fontSize: ScreenUtil().setSp(34), color: Colors.white)))),
+            Expanded(child: Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.only(top: ScreenUtil().setHeight(10)),
+              child: Text('附加功能：申请通告、管理模特', style: TextStyle(fontSize: ScreenUtil().setSp(24), color: Colors.white)))
+            )
+          ])
+        ),
+        Container(
+          height: ScreenUtil().setHeight(162),
+          width: ScreenUtil().setWidth(694),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(ScreenUtil().setWidth(12))),
+            gradient: LinearGradient(colors: [Color(0xFFFF7B0F), Color(0xFFFFDF6B)], begin: FractionalOffset(0, 0), end: FractionalOffset(1, 1))
+          ),
+          padding: EdgeInsets.symmetric(horizontal: ScreenUtil().setWidth(32), vertical: ScreenUtil().setHeight(15)),
+          margin: EdgeInsets.symmetric(vertical: ScreenUtil().setHeight(20)),
+          child: Column(children: <Widget>[
+            Expanded(child: Container(alignment: Alignment.centerLeft, child: Text('认证其他职业', style: TextStyle(fontSize: ScreenUtil().setSp(34), color: Colors.white)))),
+            Expanded(child: Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.only(top: ScreenUtil().setHeight(10)),
+              child: Text('附加功能：申请通告、发布作品、发布通告', style: TextStyle(fontSize: ScreenUtil().setSp(24), color: Colors.white)))
+            )
+          ])
         )
-      ],),
+      ]),
     );
   }
 }
